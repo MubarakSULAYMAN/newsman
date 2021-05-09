bc3c65298080d2f06e23aed1e3b17b75527b1bc4
MubarakSULAYMAN <14-30gr092@unilorin.edu.ng>
2021-05-09 21:06:40 +0100

    minor fix :adhesive_bandage:

--- src/utils/services/RequestService.js
+++ src/utils/services/RequestService.js
@@ -8,7 +8,7 @@ const apiClient = axios.create({
   withCredentials: false,
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'https://news-man.netlify.app/',
+    'Access-Control-Allow-Origin': 'https://newsapi.org/v2',
   },
 });
 
